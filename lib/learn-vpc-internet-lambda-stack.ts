01fbf526099f4c98e4792ec99c0910eb56d237b8
Eric C <oec88p@gmail.com>
2024-10-06 09:44:47 +0800

    init

--- lib/learn-vpc-internet-lambda-stack.ts
+++ lib/learn-vpc-internet-lambda-stack.ts
@@ -1,16 +1,51 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
 
 export class LearnVpcInternetLambdaStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a VPC
+    const vpc = new ec2.Vpc(this, 'WeatherVPC', {
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LearnVpcInternetLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a Lambda function
+    const weatherLambda = new lambda.Function(this, 'WeatherLambdaFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
+      vpc: vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      environment: {
+        OPENWEATHERMAP_API_KEY: '89afa2bcea3e53e276752562f91f6372',
+      },
+    });
+
+    // Allow the Lambda function to access the internet
+    weatherLambda.connections.allowToAnyIpv4(ec2.Port.tcp(443));
+
+    // Output the Lambda function ARN
+    new cdk.CfnOutput(this, 'WeatherLambdaArn', {
+      value: weatherLambda.functionArn,
+      description: 'The ARN of the Weather Lambda function',
+    });
   }
 }
--- test/learn-vpc-internet-lambda.test.ts
+++ test/learn-vpc-internet-lambda.test.ts
@@ -1,17 +1,78 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as LearnVpcInternetLambda from '../lib/learn-vpc-internet-lambda-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as WeatherLambda from '../lib/learn-vpc-internet-lambda-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/learn-vpc-internet-lambda-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new LearnVpcInternetLambda.LearnVpcInternetLambdaStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe('LearnVpcInternetLambdaStack', () => {
+  let app: cdk.App;
+  let stack: WeatherLambda.LearnVpcInternetLambdaStack;
+  let template: Template;
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new WeatherLambda.LearnVpcInternetLambdaStack(app, 'MyTestStack');
+    template = Template.fromStack(stack);
+  });
+
+  test('VPC Created', () => {
+    template.resourceCountIs('AWS::EC2::VPC', 1);
+    template.hasResourceProperties('AWS::EC2::VPC', {
+      CidrBlock: '10.0.0.0/16',
+      EnableDnsHostnames: true,
+      EnableDnsSupport: true,
+      InstanceTenancy: 'default',
+      Tags: [
+        { Key: 'Name', Value: 'MyTestStack/WeatherVPC' },
+      ],
+    });
+  });
+
+  test('Lambda Function Created', () => {
+    template.resourceCountIs('AWS::Lambda::Function', 1);
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      Handler: 'index.handler',
+      Runtime: 'nodejs20.x',
+      Environment: {
+        Variables: {
+          OPENWEATHERMAP_API_KEY: '89afa2bcea3e53e276752562f91f6372',
+        },
+      },
+    });
+  });
+
+  test('Lambda Function in VPC', () => {
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      VpcConfig: {
+        SecurityGroupIds: [
+          {
+            'Fn::GetAtt': [
+              'WeatherLambdaFunctionSecurityGroup6A1B5B66',
+              'GroupId',
+            ],
+          },
+        ],
+        SubnetIds: [
+          {
+            Ref: 'WeatherVPCPrivateSubnet1Subnet02ECA406',
+          },
+          {
+            Ref: 'WeatherVPCPrivateSubnet2SubnetACF24B49',
+          },
+        ],
+      },
+    });
+  });
+
+  test('NAT Gateway Created', () => {
+    template.resourceCountIs('AWS::EC2::NatGateway', 1);
+  });
+
+  test('Internet Gateway Created', () => {
+    template.resourceCountIs('AWS::EC2::InternetGateway', 1);
+  });
+
+  test('Lambda Function Output Created', () => {
+    template.hasOutput('WeatherLambdaArn', {
+      Description: 'The ARN of the Weather Lambda function',
+    });
+  });
 });
